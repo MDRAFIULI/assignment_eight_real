45729cafc550ce05f6e63b90ec1173a0fec486bb
RafiUlI <ctgrafi150@gmail.com>
2022-03-26 11:42:37 +0600

    first commit

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import img from '../../images/pic1.PNG'
 
 const Shop = () => {
     const products = [
-        { name: 'plus manner', id: 1, img: 'images/pic1.png', price: 12000 }
+        { name: 'deer cycle', id: 1, img: img, price: 12000 },
+        { name: 'brix bray', id: 2, img: img, price: 12000 },
+        { name: 'super co', id: 3, img: img, price: 12000 },
+        { name: 'lemon brive', id: 4, img: img, price: 12000 },
+        { name: 'dx 5', id: 5, img: img, price: 12000 },
+        { name: 'sea hand', id: 6, img: img, price: 12000 },
+        { name: 'winner pro', id: 7, img: img, price: 12000 },
+        { name: 'jhet plus', id: 8, img: img, price: 12000 },
+        { name: 'new ever', id: 9, img: img, price: 12000 }
     ]
     return (
         <div>
+            <h1>hello</h1>
             <img src={products.img} alt="" />
+            <img src={img} alt="" />
+            <img src="../../images/pic2.jpg" alt="" />
         </div>
     );
 };
